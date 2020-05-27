6dabe741cce2114c30e8c207503f21cb9e3b1c8b
jumper86 <jumper86@126.com>
2020-05-27 14:47:49 +0800

    临时保存

--- interf/operation.go
+++ interf/operation.go
@@ -38,6 +38,5 @@ const (
 
 //操作接口
 type Operation interface {
-	//Init(direct bool, params []interface{}) bool //direct 表示操作方向, true　表示　编码/压缩/加密，　false 表示　解码/解压/解密
 	Operate(direct int8, input interface{}, output interface{}) (bool, error)
 }
