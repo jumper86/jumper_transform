8f47bee7f2629d13b00f0bcb8ef35a3ebd405d76
jumper86 <jumper86@126.com>
2020-06-01 12:05:02 +0800

    添加部分单元测试，未完

---
+++ impl/encrypt/encrypt_sha1_test.go
@@ -0,0 +1,12 @@
+package encrypt_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	//. "github.com/onsi/gomega"
+	//
+	//. "github.com/jumper86/jumper_transform/impl/encrypt"
+)
+
+var _ = Describe("EncryptSha1", func() {
+
+})
